762ce040494021f95b6ad8e611f5b52b03574a79
SuperScary <wylanshoemaker@gmail.com>
2024-11-15 14:12:19 -0700

    Fixed instantiated sides for client and server

--- src/main/java/com/example/examplemod/core/ModClient.java
+++ src/main/java/com/example/examplemod/core/ModClient.java
@@ -2,8 +2,11 @@ package com.example.examplemod.core;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.level.Level;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
 
+@Mod(value = com.example.examplemod.core.Mod.MOD_ID, dist = Dist.CLIENT)
 public class ModClient extends ModBase {
 
     public ModClient (IEventBus eventBus) {
--- src/main/java/com/example/examplemod/core/ModServer.java
+++ src/main/java/com/example/examplemod/core/ModServer.java
@@ -1,8 +1,11 @@
 package com.example.examplemod.core;
 
 import net.minecraft.world.level.Level;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
 
+@Mod(value = com.example.examplemod.core.Mod.MOD_ID, dist = Dist.DEDICATED_SERVER)
 public class ModServer extends ModBase {
 
     public ModServer (IEventBus eventBus) {
